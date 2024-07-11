15b30315d7c4e099017f5dc528180032236d798d
caleb <kalejaiyecaleb@gmail.com>
2024-07-11 16:32:27 +0100

    commit

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,19 +6,19 @@ import Discount from "./Discount";
 
 const NavBar = () => {
   return (
-    <section className="">
+    <section className="flex flex-col-reverse">
         <div className="flex justify-between items-center bg-red-300 p-[10px]">
             <img className="" src={logo} width={100} alt="Logo" />
             <div>
-                <ul className="flex">
+                <ul className="lg:flex hidden">
                     {NavLinks.map((NavLinks) => (
                         <li key={NavLinks.id} className="pl-[30px] text-[12px]"><a href="" className="">{NavLinks.title}</a></li>
                     ))}
                 </ul>
             </div>
-            <div className="flex relative">
-                <Search className="absolute right-[220px] top-[6px] z-10 font-bold" size={12} />
-                <input className="rounded-[50px] w-[150px] px-[10px] pl-[25px] text-[10px] border-none outline-none relative z-0 py-[5px] font-bold" placeholder="Search" type="text" />
+            <div className="flex lg:relative">
+                <Search className="lg:absolute right-[220px] top-[6px] z-10 font-bold" size={12} />
+                <input className="rounded-[50px] w-[150px] px-[10px] pl-[25px] text-[10px] border-none outline-none relative z-0 py-[5px] font-bold lg:flex hidden" placeholder="Search" type="search" />
 
                 <div className="flex ml-[50px] items-center">
                     <Heart size={15} />
